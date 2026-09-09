9d80fe7b68cc5bf532efa12932e8ace66f1b457e
jsanchez-2025135 <jsanchez-2025135@kinal.edu.gt>
2026-09-08 23:56:39 -0600

    feat: agregar nuevas rutas de navegacion en app.routes.ts

--- control-gastos/front/src/app/app.routes.ts
+++ control-gastos/front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { IngresosComponent } from './features/ingresos/ingresos.component';
+import { EgresosComponent } from './features/egresos/egresos.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
@@ -10,7 +11,8 @@ export const routes: Routes = [
 
   // Ruta protegida: cualquier usuario autenticado (Admin o User)
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
- { path: 'ingresos', component: IngresosComponent, canActivate: [authGuard] },
+  { path: 'ingresos', component: IngresosComponent, canActivate: [authGuard] },
+  { path: 'egresos', component: EgresosComponent, canActivate: [authGuard] },
   // Ejemplo de ruta protegida SOLO para Admin (lista para cuando exista la vista)
   // { path: 'admin', component: AdminComponent, canActivate: [authGuard], data: { role: 'admin' } },
 
